fc9666bc8792e378a65211d47dd38008c604c052
A <augusthb@kth.se>
2022-01-26 18:41:39 +0100

    Spring Boot React-Redux

---
+++ src/main/java/com/IV1201VT221/IV1201/Iv1201Application.java
@@ -0,0 +1,13 @@
+package com.IV1201VT221.IV1201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iv1201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iv1201Application.class, args);
+	}
+
+}
