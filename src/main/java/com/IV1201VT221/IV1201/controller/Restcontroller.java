fdc7e2409e495addf0d23d5eb40cfead0d3d28bc
nikita <nikita@kth.se>
2022-01-28 15:34:26 +0100

    test pathing

--- src/main/java/com/IV1201VT221/IV1201/controller/Restcontroller.java
+++ src/main/java/com/IV1201VT221/IV1201/controller/Restcontroller.java
@@ -1,8 +1,13 @@
 package com.IV1201VT221.IV1201.controller;
 
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Restcontroller {
+    @GetMapping("/test")
+    public String indexTest(){
+        return "Connection working";
+    }
+
 }
