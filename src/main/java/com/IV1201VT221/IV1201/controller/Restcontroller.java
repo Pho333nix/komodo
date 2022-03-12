fff6520e3ffddbd3aa20802d686c2cb8ced80292
nikita <nikita@kth.se>
2022-03-12 18:28:18 +0100

    changed privs for available endpoint

--- src/main/java/com/IV1201VT221/IV1201/controller/Restcontroller.java
+++ src/main/java/com/IV1201VT221/IV1201/controller/Restcontroller.java
@@ -45,13 +45,6 @@ public class Restcontroller {
         this.databaseservice = databaseservice;
     }
 
-    /*
-    @RequestMapping(value = "/api/cred/{username}", method = RequestMethod.GET)
-    public String[] getCred(@PathVariable String username) throws UsernameNotFoundException {
-        String[] cred = new String[2];
-        cred = databaseservice.getCredentials(username);
-        return cred;
-    }*/
 
     /**
      * Get all person ids that are available in the specified time period
@@ -68,10 +61,10 @@ public class Restcontroller {
         try{
             roleId = databaseservice.getRoleId(email);
         }catch(Exception e){
-            logger.error("unable to get roleid");
+            logger.error("unable to get roleid", e);
             return ResponseEntity.ok("unable to get roleid for user");
         }
-        if(roleId == 1 || roleId == 2){
+        if(roleId == 1){
             try{
                 return ResponseEntity.ok(databaseservice.getAvailability(startDate, endDate));
             }catch(Exception e){
@@ -96,7 +89,7 @@ public class Restcontroller {
             userId = databaseservice.getUserId(email);
             roleId = databaseservice.getRoleId(email);
         }catch(Exception e){
-            logger.error("Unable to get userid");
+            logger.error("Unable to get userid", e);
             return ResponseEntity.ok("unable to get user");
         }
         if(roleId == 2){
@@ -105,7 +98,7 @@ public class Restcontroller {
                         app.getExperience());
                 return ResponseEntity.ok("Uploaded your application successfully");
             }catch(Exception e){
-                logger.error("unable to insert availability into database");
+                logger.error("unable to insert availability into database", e);
                 return ResponseEntity.ok("unable to insert availability into db");
             }
         }
@@ -124,14 +117,14 @@ public class Restcontroller {
         try{
             roleId = databaseservice.getRoleId(email);
         }catch(Exception e){
-            logger.error("user not in db/controller");
+            logger.error("user not in db/controller", e);
             return ResponseEntity.badRequest().body("user does not exist");
         }
         if(roleId == 2){
             try{
                 return ResponseEntity.ok(databaseservice.getAllCompetence());
             }catch(Exception e){
-                logger.error("Error accessing competences");
+                logger.error("Error accessing competences", e);
                 return ResponseEntity.badRequest().body("Could not get competence");
             }
         }else{
